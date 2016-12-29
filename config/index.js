dc3bc0c6c83245584bdf8d01640dea3dae835913
Trygve Lie <post@trygve-lie.com>
2016-12-30 00:42:39 +0100

    removed unused config

--- config/index.js
+++ config/index.js
@@ -42,31 +42,7 @@ const conf = convict({
         default: 'info',
         env: 'NODE_LOG_CONSOLE_LEVEL',
         arg: 'log-console-level',
-    },
-
-    mandrillApiKey: {
-        doc: 'The Mandrill API key',
-        format: '*',
-        default: false,
-        env: 'MANDRILL_API_KEY',
-        arg: 'mandrill-api-key',
-    },
-
-    stripeSecretApiKey: {
-        doc: 'Stripe secret API key',
-        format: '*',
-        default: false,
-        env: 'STRIPE_SECRET_API_KEY',
-        arg: 'stripe-secret-api-key',
-    },
-
-    stripePublishableApiKey: {
-        doc: 'Stripe publishable API key',
-        format: '*',
-        default: false,
-        env: 'STRIPE_PUBLISH_API_KEY',
-        arg: 'stripe-publishable-api-key',
-    },
+    }
 
 });
 
