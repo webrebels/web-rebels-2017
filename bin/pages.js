307558b513b27bbba7b8d9d7fe9a51cd8657c7f3
Max Brosnahan <maximilianbrosnahan@gmail.com>
2017-04-17 16:30:55 +0200

    Add basic schedule content

--- bin/pages.js
+++ bin/pages.js
@@ -5,6 +5,7 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 
+const day1 = 'Thursday, 2 June';
 const talks = [
     {
         title: 'Intelligent Design: Application Architecture of the Human body',
@@ -15,12 +16,37 @@ const talks = [
             twitter: 'twitter',
             github: 'github',
             iconUrl: '/img/avatar-placeholder.jpg'
-        }
+        },
+        date: day1,
+        time: '09:45'
     }
 ]
 talks.push(talks[0])
 talks.push(talks[0])
 talks.push(talks[0])
+
+const days = {
+    [day1]: {
+        description: 'Welcome to the first day of the conference',
+        schedule: [
+          {
+              title: 'Registration and breakfast',
+              details: 'As the tradition goes, no need for that hotel breakfast! Join us for a Rebel breakfast instead.',
+              time: '08:00'
+          }
+        ]
+    }
+}
+
+talks.forEach((talk) => {
+    days[talk.date].schedule.push({title: talk.title, details: talk.description, time: talk.time})
+})
+
+days[day1].schedule.push({
+    title: '',
+    details: 'Conference dinner and social gathering right next door to the venue.',
+    time: '18:00'
+})
 
 const svg = fs.readFileSync(path.resolve(__dirname, '../src/img/wr_letterlogo.svg'), {
     encoding: 'utf8',
@@ -32,6 +58,7 @@ router.get('/', (req, res) => {
         header: 'penthouse',
         css: req.css,
         talks: talks,
+        days: days,
         svg: svg
     });
 });
