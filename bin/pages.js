22889fe60ce29a248d61c41c1d5f3306721542f2
Max Brosnahan <maximilianbrosnahan@gmail.com>
2017-04-29 16:55:36 +0200

    Lint js

--- bin/pages.js
+++ bin/pages.js
@@ -16,26 +16,26 @@ const talks = [
             intro: 'Brain thinks code, art and jokes are a grand slam, and tries to do something something something.',
             twitter: 'twitter',
             github: 'github',
-            iconUrl: '/img/avatar-placeholder.jpg'
+            iconUrl: '/img/avatar-placeholder.jpg',
         },
         date: day1,
-        time: '09:45'
-    }
-]
-talks.push(talks[0])
-talks.push(talks[0])
-talks.push(talks[0])
+        time: '09:45',
+    },
+];
+talks.push(talks[0]);
+talks.push(talks[0]);
+talks.push(talks[0]);
 
 const days = {
     [day1]: {
         description: 'Welcome to the first day of the conference',
         schedule: [
-          {
-              title: 'Registration and breakfast',
-              details: 'As the tradition goes, no need for that hotel breakfast! Join us for a Rebel breakfast instead.',
-              time: '08:00'
-          }
-        ]
+            {
+                title: 'Registration and breakfast',
+                details: 'As the tradition goes, no need for that hotel breakfast! Join us for a Rebel breakfast instead.',
+                time: '08:00',
+            },
+        ],
     },
     [day2]: {
         description: 'Conference day #2, with 8 more talks starting at 09:00.',
@@ -43,23 +43,23 @@ const days = {
             {
                 title: 'Registration and breakfast',
                 details: 'As the tradition goes, no need for that hotel breakfast! Join us for a Rebel breakfast instead.',
-                time: '08:00'
-            }
-        ]
-    }
-}
+                time: '08:00',
+            },
+        ],
+    },
+};
 
 talks.forEach((talk) => {
-    days[talk.date].schedule.push({title: talk.title, details: talk.description, time: talk.time});
-})
+    days[talk.date].schedule.push({ title: talk.title, details: talk.description, time: talk.time });
+});
 
 days[day1].schedule.push({
     title: '',
     details: 'Conference dinner and social gathering right next door to the venue.',
-    time: '18:00'
-})
+    time: '18:00',
+});
 
-function readFile (pathToFile) {
+function readFile(pathToFile) {
     return fs.readFileSync(
         path.resolve(__dirname, pathToFile),
         { encoding: 'utf8' }
@@ -69,25 +69,24 @@ function readFile (pathToFile) {
 const wrSvg = readFile('../src/img/wr_letterlogo.svg');
 const fistSvg = readFile('../src/img/raised-fist.svg');
 const jsSvg = readFile('../src/img/js_logo.svg');
-const scrollScript = readFile('../src/js/zenscroll-min.js')
-let css
+const scrollScript = readFile('../src/js/zenscroll-min.js');
+let css;
 if (process.env.NODE_ENV === 'production') {
     css = `<style>${readFile('../public/main.css')}</style>`;
 } else {
     css = '<script src="http://localhost:8080/main.js"></script>';
-
 }
 router.get('/', (req, res) => {
     res.render('index', {
         pageTitle: 'Web Rebels ☠ Oslo ☠ June 2017',
         header: 'penthouse',
         css: req.css,
-        talks: talks,
-        days: days,
-        wrSvg: wrSvg,
-        fistSvg: fistSvg,
-        jsSvg: jsSvg,
+        talks,
+        days,
+        wrSvg,
+        fistSvg,
+        jsSvg,
         cssLink: css,
-        scrollScript
+        scrollScript,
     });
 });
