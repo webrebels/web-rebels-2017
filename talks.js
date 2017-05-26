0ba9ad375849daacb9a0c002c24cc44358ca45e0
nle <nolooseends@users.noreply.github.com>
2017-05-26 21:24:13 +0200

    Added the last talk info
    Added the last talk info

--- talks.js
+++ talks.js
@@ -156,8 +156,8 @@ That’s mostly it.`,
     time: '09:45',
   },
   {
-    title: 'TBA',
-    description: '',
+    title: 'Can You Read Me? Creative JavaScript to Make Computer Science Fun',
+    description: 'Two web developers who are full of curiosity to learn all about machines and code met at a meetup. Unsatisfied with traditional computer science textbook and what we felt like not so learner friendly software engineering landscape, we decided to learn by working together in the format we enjoy the most - creative project.<br/><br/>We are building machines (both software and hardware) in JavaScript to send secret messages to each other. Different forms of data blob are exchanged periodically as a message. In order to read those messages, each has to learn and build a system to decode the data.<br/><br/>This talk will tell the story of the machines, their parts, and the possibilities of remote creative collaboration. You’ll come away with fresh inspiration on how JavaScript can be used to learn computer science concepts the enjoyable way.',
     presenters: [
       {
         name: 'Mariko Kosaka',
